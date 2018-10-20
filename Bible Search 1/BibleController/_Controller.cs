890ea456fe840bcb824c308e1d5c033aabe02ed9
Josh <jwithee@gmail.com>
2018-10-19 21:49:35 -0400

    refactoring and cleanup

--- Bible Search 1/BibleController/_Controller.cs
+++ Bible Search 1/BibleController/_Controller.cs
@@ -8,25 +8,27 @@
  *****************************************************************
  */
 
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Linq;
 using System.Windows.Forms;
 
 namespace BibleController
 {
     public class _Controller
     {
-        private string bookList; 
-        private string bookPath; 
+        private string bookList;
+        private string bookPath;
         public _Controller(string _JSONBiblePath)
         {
-            if (!"".Equals(_JSONBiblePath)){
+            if (!string.IsNullOrEmpty(_JSONBiblePath))
+            {
                 bookList = _JSONBiblePath + "\\books.json";
                 bookPath = _JSONBiblePath + "\\";
             }
         }
-        
+
         public List<BibleBooks> books = new List<BibleBooks>();
 
         // Get list of Bible books from books.json
@@ -43,39 +45,26 @@ namespace BibleController
                 string text = System.IO.File.ReadAllText(bookList);
                 books = JsonConvert.DeserializeObject<List<BibleBooks>>(text);
                 return books;
-            }catch(Exception ex)
+            }
+            catch (Exception ex)
             {
-                MessageBox.Show(ex.Message + Environment.NewLine + 
+                MessageBox.Show(ex.Message + Environment.NewLine +
                     "Check your JSONBible path setting.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return null;
             }
         }
-        
+
         // Get single verse from specified book, chapter
         public string GetVerse(string _name, int _chap, int _verse)
         {
-            string tmp = "";
-            List<BookText> texts = new List<BookText>();
+            BibleBooks book = books.First(b => b.bibleBook.BookName.Equals(_name));
 
-            foreach(BibleBooks book in books)
-            {
-                if (book.bibleBook.BookName.Equals(_name))
-                {
-                    texts = GetChapters(bookPath + "\\" + book.bibleBook.BookFilePath);
-                    break;
-                }
-            }
-            foreach(BookText text in texts)
-            {
-                if(text.chapter == _chap && text.verse == _verse) {
-                    tmp = text.text.Trim();
-                    break;
-                }
-            }
-            texts = null;
-            return tmp;
+            return GetChapters(bookPath + "\\" + book.bibleBook.BookFilePath)
+                   .Find(t => t.chapter == _chap && t.verse == _verse)
+                   .text
+                   .Trim();
         }
-        
+
         // Get all text for spcified book
         public List<BookText> GetChapters(string book)
         {
@@ -85,37 +74,29 @@ namespace BibleController
                 List<BookText> texts = JsonConvert.DeserializeObject<List<BookText>>(text);
                 return texts;
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 MessageBox.Show("Error: " + ex.Message + Environment.NewLine + book);
             }
             return null;
         }
-    
+
         // Get .json file path for book
         public string GetBookPath(string bookTitle)
         {
-            foreach (BibleBooks book in books)
-            {
-                if (book.bibleBook.BookName.Equals(bookTitle))
-                {
-                    return "\\" + book.bibleBook.BookFilePath;
-                }
-            }
-            return "[error]";
+            string bookFilePath = books.Find(b => b.bibleBook.BookName == bookTitle)?.bibleBook.BookFilePath;
+
+            if (bookFilePath != null)
+                return "\\" + bookFilePath;
+            else
+                return "[error]";
         }
 
         // Get display title
         public string GetBookTitle(string bookName)
         {
-            foreach (BibleBooks book in books)
-            {
-                if (book.bibleBook.BookName.Equals(bookName))
-                {
-                    return book.bibleBook.DisplayTitle;
-                }
-            }
-            return bookName;
+            string title = books.Find(b => b.bibleBook.BookName == bookName)?.bibleBook.DisplayTitle;
+            return title ?? bookName;
         }
 
         // Search for text and return a list for ListBox control
@@ -128,24 +109,12 @@ namespace BibleController
                 foreach (BibleBooks book in books)
                 {
                     // load full book
-                    string tbookPath = bookPath;
-                    tbookPath += "\\" + this.GetBookPath(book.bibleBook.BookName);
-                    List<BookText> texts = GetChapters(tbookPath);
+                    string tbookPath = bookPath + "\\" + this.GetBookPath(book.bibleBook.BookName);
+                    IEnumerable<BookText> texts = GetChapters(tbookPath).Where(t => t.text.Contains(_searchText));
                     foreach (BookText text in texts)
                     {
-                        if (text.text.Contains(_searchText))
-                        {
-                            string txt;
-                            if (text.text.Length > 60)
-                            {
-                                txt = text.text.Substring(0, 60);
-                            }
-                            else
-                            {
-                                txt = text.text;
-                            }
-                            tList.Add(book.bibleBook.BookName + ":" + text.chapter + ":" + text.verse + ":" + txt + "...");
-                        }
+                        string txt = text.text.Substring(0, Math.Min(text.text.Length, 60));
+                        tList.Add($"{book.bibleBook.BookName}:{text.chapter}:{text.verse}:{txt}...");
                     }
                 }
             }
